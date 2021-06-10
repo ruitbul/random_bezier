1fc5e4f80840c3c25a76de08ea22a5b33cc3ad37
ruitbull <robbybullock@icloud.com>
2021-06-10 15:41:15 -0700

    Third Commit-Added JS Translation

---
+++ random_bezier_js.js
@@ -0,0 +1,96 @@
+let colArray = [];
+
+
+
+  colArray.push(color(29,30,31));
+  colArray.push(color(62,73,84)); 
+  colArray.push(color(189, 8, 59)); 
+  colArray.push(color(146, 137, 7));
+  colArray.push(color(0, 56, 168));
+
+
+let xloc = 0;
+let grid = 60;
+let margin = 10;
+
+function setup(){
+  createCanvas(800, 600);
+}
+  
+  
+function draw(){
+  background(15, 20, 30);
+  xloc += 0.03;
+  let d = grid* 0.001;
+  
+  for (i = margin; i<=width-margin; i+= grid) {
+    for ( j = margin; j<=height-margin; j+= grid) {
+      var colArrayNum = random(5);
+      stroke(colArray[colArrayNum]);
+      strokeWeight(random(4));     
+
+  
+  push();
+  translate(i, j);
+  noFill();
+  let noise1 = noise(xloc);
+  let noise2 = noise(xloc);
+  let noise3 = noise(xloc);
+  let noise4 = noise(xloc);
+  let noise5 = noise(xloc);
+  let noise6 = noise(xloc);
+  let noise7 = noise(xloc);
+  let noise8 = noise(xloc);
+  let noise9 = noise(xloc);
+  let noise10 = noise(xloc);
+  
+  
+  let h = randomGaussian();
+  let h1 = h * mouseX + random(15);
+  let h2 = h * mouseX + random(31);
+  let h3 = h * mouseX + random(24);
+  let h4 = h * mouseX + random(60);
+  let h5 = h * mouseX;
+  let h6 = h * mouseX + random(90);
+  
+  //let h = randomGaussian();
+  //let h1 = h * random(1500);
+  //let h2 = h * random(3100);
+  //let h3 = h * random(2400);
+  //let h4 = h * random(1200);
+  //let h5 = h * random(2200);
+  //let h6 = h * random(1200);
+  
+
+  //let = map(noise1, 0, 1, 0, 150);
+  //let = map(noise2, 0, 1, 0, 160);
+  //let = map(noise3, 0, 1, 0, 170);
+  //let = map(noise4, 0, 1, 0, 180);
+  //let = map(noise5, 0, 1, 0, 165);
+  //let = map(noise6, 0, 1, 0, 319);
+  //let = map(noise7, 0, 1, 0, 249);
+  //let = map(noise8, 0, 1, 0, 122);
+  //let = map(noise9, 0, 1, 0, 226);
+  //let = map(noise10, 0, 1, 0, 12);
+  
+  noise1 = map(noise1, 0, 1, 0, mouseX);
+  noise2 = map(noise2, 0, 1, 0, -mouseX);
+  noise3 = map(noise3, 0, 1, 0, 170);
+  noise4 = map(noise4, 0, 1, 0, 180);
+  noise5 = map(noise5, 0, 1, 0, 165);
+  noise6 = map(noise6, 0, 1, 0, 319);
+  noise7 = map(noise7, 0, 1, 0, 249);
+  noise8 = map(noise8, 0, 1, 0, 122);
+  noise9 = map(noise9, 0, 1, 0, 226);
+  noise10 = map(noise10, 0, 1, 0, 12);
+  
+
+  beginShape();
+  vertex(100, 100);
+  quadraticVertex(noise1, noise2, noise3, noise4);
+  bezierVertex(h1, h2, h3, h4, h5, h6);
+  endShape(CLOSE);
+  pop();
+    }
+  }
+}
